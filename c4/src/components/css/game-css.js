cb55921dd7d32a1d072a370a59af8f35bca2b831
Maria Paula Herrero <mpaulaherrero@gmail.com>
2022-12-27 16:30:31 -0400

    cambio color

--- c4/src/components/css/game-css.js
+++ c4/src/components/css/game-css.js
@@ -3,7 +3,7 @@ import {css} from 'lit';
 export const gameCSS = css`
             :host {
                 width: 100%;
-                --primary-color: #8009ff;
+                --primary-color: #09a5ff;
                 display: flex;
                 flex-direction: column;
                 justify-content: stretch;
